d0087e275970f655e9df6c4317312dc4118cb166
MakotoHirakawa <>
2024-08-16 23:21:07 +0900

    プロジェクト一覧を修正

--- app/nextodo/page.tsx
+++ app/nextodo/page.tsx
@@ -1,36 +1,27 @@
 import LinkCardWithAvatar from "@/components/cards/LinkCardWithAvatar";
-import fetchGet from "@/util/fetch/get";
+import { getProjects } from "@/features/projects/actions";
+import { ProjectCard } from "@/features/projects/type";
 
 export default async function Page() {
 
-  const ProjectList = async () => {
-    try{
-      const projects = await fetchGet({
-        url: '/tms/projects/',
-        hasToken: true,
-      });
-      return await projects.map((project: any) => (
-        <LinkCardWithAvatar
-          key={project.id}
-          object={{
-            href: `/nextodo/${project.id}`,
-            title: project.name,
-            text: project.description,
-            url: project.image_url
-          }}
-        />
-      ));
-    }catch(e){
-      return <p className="text-error">エラーが発生しました。</p>;
-    }
-  }
+  const projectCards:ProjectCard[] = await getProjects();
 
   return (
     <>
       <div className="p-8 w-full h-full">
         <h1 className="text-4xl text-center mb-10">This is ProjectList</h1>
           <div className="md:flex md:flex-wrap md:flex-row flex-col gap-4 mx-auto justify-between max-w-3xl">
-            <ProjectList />
+            {projectCards.map((projectCard:ProjectCard) => (
+              <LinkCardWithAvatar
+                key={projectCard.id}
+                object={{
+                  href: `/nextodo/${projectCard.id}`,
+                  title: projectCard.name,
+                  text: projectCard.description,
+                  url: projectCard.image_url
+                }}
+              />
+            ))}
           </div>
       </div>
     </>
